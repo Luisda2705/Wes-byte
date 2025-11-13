2ebdff923d32ea03aab9dcccb58c70942b7f5641
Luisda2705 <luisda27055@gmail.com>
2025-11-13 15:41:48 -0600

    15:41:48 - 13/11/25

---
+++ Server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, World! 🌍\n');
+    } else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('About Us 📖\n');
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Page Not Found 🚨\n');
+    }
+});
+
+const port = 3000;
+const hostname = '127.0.0.1';
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/ 🎉`);
+});
