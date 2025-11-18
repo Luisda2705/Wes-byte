6926fd6044e266da1920030ffba802f46227d7b8
Luisda2705 <luisda27055@gmail.com>
2025-11-17 23:00:34 -0600

    23:00:34 - 17/11/25

---
+++ Database.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb');
+const uri = "<Your Connection String>";
+const client = new MongoClient(uri);
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } finally {
+        await client.close();
+    }
+}
+
+
+run().catch(console.error);
